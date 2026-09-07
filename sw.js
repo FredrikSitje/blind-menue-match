718f04c2b7e7951283cac91cd69f97cc2fd6cf10
Fredrik <fredrik@MacBookPro.home>
2026-09-07 22:40:37 +0200

    Fetch fresh assets when installing a new service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bmm-v4";
+const CACHE = "bmm-v5";
 const ASSETS = [
   "./",
   "./index.html",
@@ -9,8 +9,9 @@ const ASSETS = [
   "./js/sync.js",
   "./manifest.webmanifest"
 ];
+// A new release must not seed its cache from stale HTTP-cached JavaScript.
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS.map((asset) => new Request(asset, { cache: "reload" })))).then(() => self.skipWaiting()));
 });
 self.addEventListener("activate", (e) => {
   e.waitUntil(
